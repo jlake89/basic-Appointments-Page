e0cb666c9a6c4401740a872ff41d7da741c08070
Joel Lake <joelcooperlake@gmail.com>
2022-04-03 16:21:26 -0700

    add pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
     event.preventDefault();
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
+    const animalInput = $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
-    
+
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
